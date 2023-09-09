21a2ecfaa9d6815a6544d8dcc5cef1dcd3f91acc
Mikhail Lukichev <dextrumm@gmail.com>
2023-09-08 23:28:33 -0600

    Homework is ready

--- src/main/java/pro/sky/mikhaillukichevcollections/exception/EmployeeAlreadyAddedException.java
+++ src/main/java/pro/sky/mikhaillukichevcollections/exception/EmployeeAlreadyAddedException.java
@@ -7,5 +7,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class EmployeeAlreadyAddedException extends RuntimeException{
     public EmployeeAlreadyAddedException(String message) {
         super(message);
+        System.out.println(message);
     }
 }
--- src/main/java/pro/sky/mikhaillukichevcollections/model/EmployeeService.java
+++ src/main/java/pro/sky/mikhaillukichevcollections/model/EmployeeService.java
@@ -3,10 +3,11 @@ package pro.sky.mikhaillukichevcollections.model;
 import pro.sky.mikhaillukichevcollections.model.Employee;
 
 import java.util.List;
+import java.util.Map;
 
 public interface EmployeeService {
     Employee addEmployee(String firstName, String lastName);
     Employee removeEmployee(String firstName, String lastName);
     Employee findEmployee(String firstName, String lastName);
-    List<Employee> displayEmployees();
+    Map<String,Employee> displayEmployees();
 }
